9706dcde315ccb2c3c05b237da8a0e63c11cedd8
philipplgh <philipp@ethereum.org>
2018-09-21 15:52:27 +0200

    add thunk middleware

--- src/API/ReduxStore.js
+++ src/API/ReduxStore.js
@@ -1,2 +1,13 @@
 import {Helpers} from './Helpers'
-export default Helpers.isMist() ? window.store.getState().nodes : window.store
+// export default Helpers.isMist() ? window.store.getState().nodes : window.store
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk'
+
+const initialState = window.store
+function mistApp(state = initialState, action) {
+  // For now, don't handle any actions
+  // and just return the state given to us.
+  return state
+}
+const store = createStore(mistApp, applyMiddleware(ReduxThunk))
+export default store
