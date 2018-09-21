56f12b3861b57e5476f7aaa77579f348228ed5af
philipplgh <philipp@ethereum.org>
2018-09-21 12:53:04 +0200

    draft mist API

--- src/API/Mist.js
+++ src/API/Mist.js
@@ -1,5 +1,21 @@
+import ipc from './Ipc'
+
 const MistApi = {
   // requestAccount: window.mist.requestAccount
+  setWindowSize(w, h){
+    ipc.send('backendAction_setWindowSize', w, h);
+  },
+  // replaces GlobalNotification
+  notification(){
+    warn:()=>{
+      /*
+          GlobalNotification.warning({
+            content: error.message || error,
+            duration: 5
+          });
+      */
+    }
+  }
 }
 
 export default MistApi
