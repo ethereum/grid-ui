47bd7e2857a08b037d466da96d3de5af5986c7fc
philipplgh <philipp@ethereum.org>
2018-09-21 16:33:35 +0200

    temporarily provide access to fs and path modules in fakeAPI - this will break browser support

--- preload.js
+++ preload.js
@@ -5,6 +5,13 @@ babelRegister({
   presets: ['env', 'react-app'],
 });
 
+const fs = require('fs')
+const path = require('path')
+
+// TODO WARNING not browser compatible -> needs to be mocked
+window.__fs = fs
+window.__path = path
+
 // fakeAPI simulates an environment with all required globals such as i18n or web3 so that the code
 // is not breaking, however the functionality will obviously not work 
 // create mocks / stubs / fakes for global variables and APIs
@@ -12,3 +19,4 @@ babelRegister({
 //moved to App.js so that it works in the browser too
 
 window.dirname = __dirname
+window.__dirname = __dirname
