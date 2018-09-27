cb6a1e550b4362a7e3dc66f5dbf2aea81139a7b9
philipp <philipplgh@gmail.com>
2018-09-27 14:00:45 +0200

    connect component to redux store

---
+++ src/containers/Dapps/DappOverview.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import "../normalize.css"
+
+import DappItem from './DappItem'
+
+class DappOverview extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render(){
+    let suggested = this.props.suggestedDapps
+    let connectedDapps = this.props.tabs
+    return (
+      <main className="dapps">
+        <h1><strong>Dapps</strong> Overview</h1>
+
+        <h2>Connected</h2>
+        <section className="card-container dapps">
+        {
+          connectedDapps.map(dapp => {
+            return <DappItem dapp={dapp}></DappItem>
+          })
+        }
+        </section>
+
+        <h2>Suggested</h2>
+        <section className="card-container dapps">
+        {
+          suggested.map(dapp => {
+            return <DappItem dapp={dapp}></DappItem>
+          })
+        }
+        </section>
+      </main>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    tabs: state.tabs,
+    suggestedDapps: state.suggestedDapps
+  };
+}
+
+export default connect(mapStateToProps)(DappOverview)
---
+++ src/containers/Wallet/Wallet.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import "../normalize.css"
+import "./Wallet.css"
+import Account from './AccountItem'
+
+class Wallet extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render(){
+    let accounts = this.props.accounts
+    return (
+    <main className="wallet">
+      <h1><strong>Accounts</strong> Overview</h1>
+    
+      <div className="wallet-actions">
+        <button className="wallet-action">create</button>
+        <button className="wallet-action">import</button>
+      </div>
+    
+      <h2>Network Main</h2>
+    
+      <div className="wallet-box-list">
+        {
+          accounts.map((account, idx) => {
+            return <Account key={account.address} account={account} idx={idx}></Account>
+          })
+        }
+      </div>              
+    </main>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    accounts: state.accounts
+  };
+}
+
+export default connect(mapStateToProps)(Wallet)
