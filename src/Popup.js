d1f1265972a98089c0e031348c921e06bf430ca5
philipplgh <philipp@ethereum.org>
2018-10-09 15:33:04 +0200

    add settings popup

--- src/Popup.js
+++ src/Popup.js
@@ -8,19 +8,21 @@ import ConnectAccount from './popups/ConnectAccount'
 import SendTransactionConfirmation from './popups/SendTransactionConfirmation'
 import SendTx from './components/SendTx'
 import TxHistory from './components/TxHistory'
-import RequestAccount from './components/RequestAccount'
+import RequestAccount from './popups/CreateAccount/RequestAccount'
+import Settings from './popups/Settings'
 
 function PopupBase({ name, popup }) {
   console.log('load content for popup: ', name)
   switch(name){
     case 'ConnectAccount':
-    case 'connectAccount':
       return <ConnectAccount popup={popup}/>
     case 'RequestAccount':
     case 'CreateAccount':
       return <RequestAccount />
     case 'SendTransactionConfirmation':
       return <SendTransactionConfirmation />
+    case 'Settings':
+      return <Settings />    
     case 'SendTx':
       return <SendTx />
     case 'TxHistory':
---
+++ src/popups/Settings/Settings.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { i18n } from '../../API'
+
+class Settings extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="popup-windows update-available row">
+        <h2>hello settings</h2>
+      </div>
+    )
+  }
+}
+
+export default Settings
