9678b3d516fdcc1782a9b7eae7796674861365de
Marc Garreau <marcdgarreau@gmail.com>
2019-02-27 12:11:14 -0700

    Tweaks theme, removes cruft (#20)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React, { Component } from 'react'
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import NavTabs from './NavTabs'
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      // light: will be calculated from palette.primary.main,
-      main: '#4185DE'
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
-    }
-    // secondary: {
-    // light: '#0066ff',
-    // main: '#0044ff',
-    // dark: will be calculated from palette.secondary.main,
-    // contrastText: '#ffcc00',
-    // },
-    // error: will use the default color
-  }
-})
+import theme from './theme'
 
 export default class NewApp extends Component {
   static displayName = 'App'
@@ -30,6 +14,7 @@ export default class NewApp extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
+        <CssBaseline />
         <NavTabs />
       </MuiThemeProvider>
     )
