d4a04a32e04a3640f842e28fd342c353afe64843
philipplgh <philipplgh@gmail.com>
2019-04-11 13:16:15 +0200

    optimize list rendering

---
+++ src/components/Nodes/ClientConfig/VersionList.jsx
@@ -0,0 +1,259 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import semver from 'semver'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import SnackbarContent from '@material-ui/core/SnackbarContent'
+import RefreshIcon from '@material-ui/icons/Refresh'
+import WarningIcon from '@material-ui/icons/Warning'
+import amber from '@material-ui/core/colors/amber'
+import Spinner from '../../shared/Spinner'
+import { setRelease } from '../../../store/client/actions'
+import VersionListItem from './VersionListItem'
+
+const lightGrey = 'rgba(0,0,0,0.25)'
+
+const styles = () => ({
+  refreshIcon: {
+    fontSize: 22,
+    color: lightGrey,
+    marginLeft: 5,
+    verticalAlign: 'middle',
+    marginBottom: 4,
+    visibility: 'hidden'
+  },
+  versionsAvailable: {
+    '&:hover': {
+      cursor: 'pointer'
+    },
+    '&:hover $refreshIcon': {
+      visibility: 'visible'
+    }
+  },
+  warning: {
+    backgroundColor: amber[700],
+    opacity: 0.9,
+    margin: '10px 0 15px 0'
+  },
+  warningIcon: {
+    fontSize: 19,
+    verticalAlign: 'middle',
+    marginBottom: 2
+  }
+})
+
+class VersionList extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func,
+    client: PropTypes.object,
+    classes: PropTypes.object
+  }
+
+  state = {
+    releases: [],
+    localReleaseCount: 0,
+    loadingReleases: false,
+    downloadError: null
+  }
+
+  componentDidMount = async () => {
+    // this.loadReleases()
+  }
+
+  componentWillReceiveProps({ client: nextClient }) {
+    let { client: oldClient } = this.props
+    if (oldClient && nextClient !== oldClient) {
+      // this.loadLocalReleases()
+      this.setState({ releases: [] })
+      this.loadReleases(nextClient)
+    }
+  }
+
+  setSelectedRelease = release => {
+    const { dispatch } = this.props
+    dispatch(setRelease({ release }))
+  }
+
+  loadReleases = async client => {
+    console.log('load releases')
+    client = client || this.props.client
+    this.setState({ loadingReleases: true })
+    console.time('load releases')
+    const releases = await client.getReleases()
+    console.timeEnd('load releases')
+    console.log('releases loaded:', releases.length)
+    this.setState({ releases, loadingReleases: false })
+    const localReleases = releases.filter(r => !r.remote)
+    this.setState({ localReleaseCount: localReleases.length })
+  }
+
+  isLocalRelease = release => {
+    return !release.remote
+  }
+
+  renderVersionsAvailable = () => {
+    const { classes } = this.props
+    const { loadingReleases, releases, localReleaseCount } = this.state
+
+    if (releases.length === 0) {
+      return <Spinner style={{ margin: '20px 0' }} />
+    }
+
+    return (
+      <div>
+        {loadingReleases && (
+          <Typography variant="h6">
+            Loading versions...
+            <RemoteReleaseLoadingSpinner size={18} thickness={4} />
+          </Typography>
+        )}
+        {!loadingReleases && (
+          <Typography
+            variant="h6"
+            onClick={this.handleRefresh}
+            classes={{ root: classes.versionsAvailable }}
+          >
+            {releases.length} versions available
+            <RefreshIcon classes={{ root: classes.refreshIcon }} />
+          </Typography>
+        )}
+        <Typography>
+          <StyledDownloadedVersions>
+            {localReleaseCount} versions downloaded
+          </StyledDownloadedVersions>
+        </Typography>
+      </div>
+    )
+  }
+
+  handleRefresh = () => {
+    this.loadReleases()
+  }
+
+  renderWarnings = () => {
+    return <div>{this.renderLatestVersionWarning()}</div>
+  }
+
+  renderLatestVersionWarning = () => {
+    const { classes, client } = this.props
+    const { releases } = this.state
+    const { release } = client
+    if (!release || !releases.length) {
+      return null
+    }
+    const latestRelease = this.allReleases()[0]
+    const latestVersion = latestRelease.version
+    const selectedVersion = release.version
+    if (semver.compare(selectedVersion, latestVersion)) {
+      return (
+        <SnackbarContent
+          classes={{ root: classes.warning }}
+          message={
+            <span>
+              <WarningIcon classes={{ root: classes.warningIcon }} /> You are
+              using an older version of Geth ({selectedVersion})<br />
+              New releases contain performance and security enhancements.
+            </span>
+          }
+          action={
+            <Button
+              onClick={() => {
+                this.handleReleaseSelected(latestRelease)
+              }}
+            >
+              Use {latestVersion}
+            </Button>
+          }
+        />
+      )
+    }
+    return null
+  }
+
+  isSelectedRelease = release => {
+    const { client } = this.props
+    if (!client.release) return false
+    return release.fileName === client.release.fileName
+  }
+
+  handleReleaseDownloaded = release => {
+    const releaseDownloaded = { ...release, remote: false }
+    const { releases, localReleaseCount } = this.state
+    const index = releases.findIndex(r =>
+      semver.lte(r.version, releaseDownloaded.version)
+    )
+    releases.splice(index, 0, releaseDownloaded)
+    this.setState({
+      releases: [...releases],
+      localReleaseCount: localReleaseCount + 1
+    })
+  }
+
+  renderVersionList = () => {
+    const { client } = this.props
+    const { releases } = this.state
+    const renderListItems = () => {
+      const list = releases.map((release, i) => {
+        return (
+          <VersionListItem
+            client={client}
+            release={release}
+            key={i}
+            handleDownloadError={downloadError =>
+              this.setState({ downloadError })
+            }
+            handleReleaseDownloaded={this.handleReleaseDownloaded}
+          />
+        )
+      })
+      return list
+    }
+    return <StyledList>{renderListItems()}</StyledList>
+  }
+
+  render() {
+    const { downloadError } = this.state
+    return (
+      <div>
+        {this.renderVersionsAvailable()}
+        {this.renderWarnings()}
+        {this.renderVersionList()}
+        {downloadError && <StyledError>{downloadError.message}</StyledError>}
+      </div>
+    )
+  }
+}
+/*
+function mapStateToProps(state) {
+  return {
+    client: state.client
+  }
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(VersionList))
+*/
+export default withStyles(styles)(VersionList)
+
+const StyledList = styled(List)`
+  min-height: 200px;
+  max-height: 300px;
+  overflow-y: scroll;
+`
+
+const RemoteReleaseLoadingSpinner = styled(Spinner)`
+  margin-left: 10px;
+`
+
+const StyledError = styled.div`
+  color: red;
+`
+
+const StyledDownloadedVersions = styled.span`
+  color: lightGrey;
+  font-size: 13px;
+  font-weight: bold;
+  text-transform: uppercase;
+`
