fc90aa94b3356fdb8cba49cca4b4822520c311c5
Ryan Ghods <ryan@ryanio.com>
2019-05-14 11:21:58 -0700

    fix: jsx-closing-tag-location conflicting with prettier formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
     'react/forbid-prop-types': 0,
     'react/jsx-closing-bracket-location': 0,
     'react/jsx-one-expression-per-line': 0,
+    'react/jsx-closing-tag-location': 0,
     'react/no-array-index-key': 0,
     'react/prefer-stateless-function': 0,
     'react/prop-types': 2,
