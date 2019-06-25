9aae3ffbd0d0ef672b9c80b7e144a01b14d1b573
Ryan Ghods <ryan@ryanio.com>
2019-06-25 16:10:51 -0700

    feat: scroll through terminal input history on up arrow and down arrow

--- src/components/Nodes/Terminal/TerminalInput.js
+++ src/components/Nodes/Terminal/TerminalInput.js
@@ -34,7 +34,9 @@ class TerminalInput extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      input: ''
+      input: '',
+      history: [''],
+      historyIndex: 0
     }
   }
 
@@ -42,12 +44,41 @@ class TerminalInput extends Component {
     this.setState({ input: event.target.value })
   }
 
+  handleKeyDown = event => {
+    // On up or down arrow, navigate through history
+    if (event.keyCode === 38 || event.keyCode === 40) {
+      event.preventDefault()
+      const { history, historyIndex } = this.state
+      let newIndex = historyIndex
+      if (event.keyCode === 38) {
+        // up arrow
+        newIndex += 1
+        if (newIndex > history.length) {
+          newIndex = 0
+        }
+      } else if (event.keyCode === 40) {
+        // down arrow
+        newIndex -= 1
+        if (newIndex < 0) {
+          newIndex = history.length
+        }
+      }
+      const input = history[history.length - newIndex]
+      this.setState({ input, historyIndex: newIndex })
+    }
+  }
+
   submit = () => {
     const { client, addNewLog } = this.props
-    const { input } = this.state
+    const { input, history } = this.state
     client.write(input)
     addNewLog(input)
-    this.setState({ input: '' })
+    this.setState({ input: '', historyIndex: 0 })
+    // Add to history if not same as last history entry
+    if (history[-1] !== input) {
+      const newHistory = [...history, input]
+      this.setState({ history: newHistory })
+    }
   }
 
   render() {
@@ -66,6 +97,7 @@ class TerminalInput extends Component {
           data-test-id="terminal-input"
           value={input}
           onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
           disabled={!isClientRunning}
           className={classes.textField}
           InputProps={{
