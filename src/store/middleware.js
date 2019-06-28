a86b3a930aecfc36004cf8d16c98331993952167
Marc Garreau <marcdgarreau@gmail.com>
2019-06-28 12:28:39 -0600

    fix: function purity

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -4,8 +4,9 @@ import { Grid } from '../API'
 export const saveSettings = store => next => async action => {
   if (action.type === 'CLIENT:SET_CONFIG') {
     const settings = await Grid.Config.getItem('settings')
-    settings[action.payload.clientName] = action.payload.config
-    await Grid.Config.setItem('settings', settings)
+    const newSettings = Object.assign({}, settings)
+    newSettings[action.payload.clientName] = action.payload.config
+    await Grid.Config.setItem('settings', newSettings)
   }
 
   return next(action)
