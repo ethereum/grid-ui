1bd5402b73c9c51ce6e6f0dad506c61c801bea3f
Marc Garreau <marcdgarreau@gmail.com>
2019-06-27 21:52:15 -0600

    fix: remove redux-persist cruft

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,37 +1,8 @@
-import omit from 'lodash/omit'
 import { combineReducers } from 'redux'
-import { createTransform, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import client, { initialClientState } from './client/reducer'
-
-const pluginTransform = createTransform((inboundState, key) => {
-  if (key === 'client') {
-    const persistablePluginData = { selected: inboundState.selected }
-
-    const plugins = Object.keys(omit(inboundState, 'selected'))
-    plugins.forEach(plugin => {
-      persistablePluginData[plugin] = {
-        ...initialClientState,
-        config: inboundState[plugin].config
-      }
-    })
-
-    return persistablePluginData
-  }
-
-  return inboundState
-})
-
-const rootPersistConfig = {
-  key: 'root',
-  storage,
-  transforms: [
-    /* pluginTransform */
-  ]
-}
+import client from './client/reducer'
 
 const rootReducer = combineReducers({
   client
 })
 
-export default persistReducer(rootPersistConfig, rootReducer)
+export default rootReducer
