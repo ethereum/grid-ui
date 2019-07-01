069a79520b05fb985116699aeee51a7a3b19715f
Ryan Ghods <ryan@ryanio.com>
2019-07-01 16:52:08 -0700

    chore: move block subscriptions to own method to not clutter onConnect method

--- src/store/client/clientService.js
+++ src/store/client/clientService.js
@@ -133,33 +133,36 @@ class ClientService {
     this.syncingSubscriptionId = null
   }
 
+  startBlockSubscriptions(client, dispatch) {
+    const startSubscriptions = async () => {
+      const result = await client.rpc('eth_syncing')
+      if (result === false) {
+        // Not syncing, start newHeads subscription
+        this.startNewHeadsSubscription(client, dispatch)
+      } else {
+        // Subscribe to syncing
+        this.startSyncingSubscription(client, dispatch)
+      }
+    }
+
+    const setLastBlock = () => {
+      client.rpc('eth_getBlockByNumber', ['latest', false]).then(block => {
+        const { number: hexBlockNumber, timestamp: hexTimestamp } = block
+        const blockNumber = Number(toNumberString(hexBlockNumber))
+        const timestamp = Number(toNumberString(hexTimestamp))
+        dispatch(newBlock(client.name, blockNumber, timestamp))
+      })
+    }
+
+    setTimeout(() => {
+      setLastBlock()
+      startSubscriptions()
+    }, 2000)
+  }
+
   onConnect(client, dispatch) {
-    // Only start block subscriptions for `client` plugins
     if (client.type === 'client') {
-      const startSubscriptions = async () => {
-        const result = await client.rpc('eth_syncing')
-        if (result === false) {
-          // Not syncing, start newHeads subscription
-          this.startNewHeadsSubscription(client, dispatch)
-        } else {
-          // Subscribe to syncing
-          this.startSyncingSubscription(client, dispatch)
-        }
-      }
-
-      const setLastBlock = () => {
-        client.rpc('eth_getBlockByNumber', ['latest', false]).then(block => {
-          const { number: hexBlockNumber, timestamp: hexTimestamp } = block
-          const blockNumber = Number(toNumberString(hexBlockNumber))
-          const timestamp = Number(toNumberString(hexTimestamp))
-          dispatch(newBlock(client.name, blockNumber, timestamp))
-        })
-      }
-
-      setTimeout(() => {
-        setLastBlock()
-        startSubscriptions()
-      }, 2000)
+      this.startBlockSubscriptions(client, dispatch)
     }
   }
 
