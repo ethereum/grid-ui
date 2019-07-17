ce30a20662399e14982a9b753881973179b393bd
Ev <ev@ethereum.org>
2019-07-17 15:59:33 -0400

    feat: Dark mode (#107)
    * feat: first iteration of dark theme
    
    * fix: lint rule exception
    
    * chore: pass themeMode via props so that grid-ui can be opened with args.scope.themeMode = 'dark' or 'light'. defaults to 'dark'

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import NavTabs from './NavTabs'
-import theme from '../theme'
+import { darkTheme, lightTheme } from '../theme'
 import HelpFab from './shared/HelpFab'
 import ErrorBoundary from './GenericErrorBoundary'
 
 export default class NewApp extends Component {
   static displayName = 'App'
 
-  static propTypes = {}
-
-  static defaultProps = {}
+  static propTypes = {
+    themeMode: PropTypes.oneOf(['dark', 'light'])
+  }
 
   render() {
+    const { themeMode } = this.props
     return (
-      <MuiThemeProvider theme={theme}>
+      <MuiThemeProvider theme={themeMode === 'light' ? lightTheme : darkTheme}>
         <CssBaseline />
         <HelpFab />
         <ErrorBoundary>
