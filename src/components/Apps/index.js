70e43287f4e828cdb1e9e2551fc3e2f3c7d9dc0f
Everton Fraga <ev@ethereum.org>
2019-07-26 15:22:23 -0400

    fix: white Apps cards on dark theme

--- src/components/Apps/index.js
+++ src/components/Apps/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import AppItem from './AppItem'
 import GridAPI from '../../API/Grid'
+import { darkTheme, lightTheme } from '../../theme'
 
 class AppsOverview extends React.Component {
   state = {}
@@ -10,19 +12,21 @@ class AppsOverview extends React.Component {
     const apps = GridAPI.AppManager ? GridAPI.AppManager.getAvailableApps() : []
 
     return (
-      <div style={{ width: '100%', paddingBottom: '64px' }}>
-        <Grid container spacing={24}>
-          {apps.length ? (
-            apps.map(app => (
-              <Grid item xs={4} key={app.name}>
-                <AppItem app={app} />
-              </Grid>
-            ))
-          ) : (
-            <span>no apps found</span>
-          )}
-        </Grid>
-      </div>
+      <MuiThemeProvider theme={darkTheme}>
+        <div style={{ width: '100%', paddingBottom: '64px' }}>
+          <Grid container spacing={24}>
+            {apps.length ? (
+              apps.map(app => (
+                <Grid item xs={4} key={app.name}>
+                  <AppItem app={app} />
+                </Grid>
+              ))
+            ) : (
+              <span>no apps found</span>
+            )}
+          </Grid>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
