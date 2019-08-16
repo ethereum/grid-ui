9934af5a6027a5e71c408af81e3c0a840342806a
Marc Garreau <marcdgarreau@gmail.com>
2019-08-16 16:18:42 -0600

    chore: remove stale files

--- preload/preload.js
+++ preload/preload.js
@@ -36,12 +36,6 @@ window._mist = {
   }
 }
 
-// fakeAPI simulates an environment with all required globals such as i18n or web3 so that the code
-// is not breaking, however the functionality will obviously not work
-// create mocks / stubs / fakes for global variables and APIs
-// let fakeAPI = require('./src/fakeAPI')
-// moved to App.js so that it works in the browser too
-
 window.__basedir = __dirname // path.join(__dirname, '..')
 
 window.__require = function(name) {
